fdf07d80e9c578e66a9ea368ab44d4a9fb26e2cf
andarpratama <andar.webdev@gmail.com>
2022-06-11 04:14:12 +0700

    heroku: update port

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
-const logging = require('./services/logging')
-const App = require("./app");
-require('dotenv').config()
+const express = require('express');
+const logging = require('./services/logging');
+const App = require('./app');
+require('dotenv').config();
 
-// => Run App 
-App.listen(process.env.PORT, () => {
-	logging.info('SERVER', `Running on port http://localhost:${process.env.PORT}`)
-})
+// => Run App
+App.listen(process.env.PORT || 3000, () => {
+  logging.info(
+    'SERVER',
+    `Running on port http://localhost:${process.env.PORT}`
+  );
+});
