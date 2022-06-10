11f3680106c929431a94d0639fcf439719bb10b9
andarpratama <andar.webdev@gmail.com>
2022-06-11 05:07:27 +0700

    heroku: update db host

--- models/Users.js
+++ models/Users.js
@@ -1,54 +1,67 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 // mongoose.set('useFindAndModify', false);
 
-const usersSchema = new Schema({
-   name: {
+const usersSchema = new Schema(
+  {
+    name: {
       type: String,
-      required: true
-   },
-   email: {
+      required: true,
+    },
+    email: {
       type: String,
-      required: true
-   },
-   password: {
+      required: true,
+      unique: true,
+    },
+    password: {
       type: String,
-      required: true
-   },
-   resource: {
+      required: true,
+    },
+    resource: {
       golds: {
-         type: Number,
-         minLength: 0,
-         maxLength: 1000,
-         default: 100
+        type: Number,
+        minLength: 0,
+        maxLength: 1000,
+        default: 100,
       },
       foods: {
-         type: Number,
-         minLength: 0,
-         maxLength: 1000,
-         default:100
+        type: Number,
+        minLength: 0,
+        maxLength: 1000,
+        default: 100,
       },
       soldiers: {
-         type: Number,
-         minLength: 0,
-         maxLength: 500,
-         default:0
+        type: Number,
+        minLength: 0,
+        maxLength: 500,
+        default: 0,
       },
       medal: {
-         type: Number,
-         default: 0
+        type: Number,
+        default: 0,
       },
-      markets: [{
-         type: mongoose.Types.ObjectId, ref: 'Market'
-      }],
-      farms: [{
-         type: mongoose.Types.ObjectId, ref: 'Farm'
-      }],
-      barracks: [{
-         type: mongoose.Types.ObjectId, ref: 'Barrack'
-      }]
-   }
-}, { timestamps: { createdAt: 'created_at', updateAt: 'update_at' } })
+      markets: [
+        {
+          type: mongoose.Types.ObjectId,
+          ref: 'Market',
+        },
+      ],
+      farms: [
+        {
+          type: mongoose.Types.ObjectId,
+          ref: 'Farm',
+        },
+      ],
+      barracks: [
+        {
+          type: mongoose.Types.ObjectId,
+          ref: 'Barrack',
+        },
+      ],
+    },
+  },
+  { timestamps: { createdAt: 'created_at', updateAt: 'update_at' } }
+);
 
-const Users = mongoose.model('Users', usersSchema)
-module.exports = Users
+const Users = mongoose.model('Users', usersSchema);
+module.exports = Users;
