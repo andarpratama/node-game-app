cef919441dbe7cd4d7784273eeae42699cc50026
andarpratama <andar.webdev@gmail.com>
2022-06-11 03:28:21 +0700

    heroku: update barrack controller in route

--- routes/barrack.route.js
+++ routes/barrack.route.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const barrackController = require('../controllers/barrack.constroller');
-const auth = require('../middlewares/authJwt')
+const barrackController = require('../controllers/barrack.controller');
+const auth = require('../middlewares/authJwt');
 const errHanddler = require('../middlewares/errorHanddler');
 
-router.use(auth.authentication)
-router.use(auth.authorization)
+router.use(auth.authentication);
+router.use(auth.authorization);
 router.get('/', barrackController.getAll);
 router.get('/:id', barrackController.getOne);
 router.post('/', barrackController.createBarrack);
 router.put('/:id', barrackController.updateBarrack);
 router.delete('/:id', barrackController.deleteBarrack);
-router.use(errHanddler)
+router.use(errHanddler);
 
-router.post('/collect/:id', barrackController.collect)
+router.post('/collect/:id', barrackController.collect);
 
 module.exports = router;
