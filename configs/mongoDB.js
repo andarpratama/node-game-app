18bcfaf363851f5a760527bcfdd8f452055f6278
andarpratama <andar.webdev@gmail.com>
2022-06-11 04:04:36 +0700

    heroku: update db host

--- configs/mongoDB.js
+++ configs/mongoDB.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose')
-const logging = require('../services/logging')
+const mongoose = require('mongoose');
 const generateResource = require('../services/generate.resource');
 const connectDB = async () => {
-   // const pathURL = "mongodb+srv://andarpratama:0ne41sGvLaRjTrCA@node-game-app.scrnm.mongodb.net/game-api-test?retryWrites=true&w=majority"
-   const pathURL = process.env.DB_HOST_LOCAL
-   const connectOption = { useNewUrlParser: true, useUnifiedTopology: true }
-   mongoose.connect(pathURL, connectOption)
+  // const pathURL = "mongodb+srv://andarpratama:0ne41sGvLaRjTrCA@node-game-app.scrnm.mongodb.net/game-api-test?retryWrites=true&w=majority"
+  const pathURL = process.env.DB_HOST;
+  const connectOption = { useNewUrlParser: true, useUnifiedTopology: true };
+  mongoose.connect(pathURL, connectOption);
 
-   // cecking mongodb
-   const db = mongoose.connection
-   db.on('error', console.error.bind(console, 'Connection error :'))
-   db.once('open', async () => {
-      generateResource()
-      // console.log(`[INFO] [DB] Message : Database connected`)
-   } )
-}
+  // cecking mongodb
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'Connection error :'));
+  db.once('open', async () => {
+    generateResource();
+    // console.log(`[INFO] [DB] Message : Database connected`)
+  });
+};
 
-module.exports = connectDB
+module.exports = connectDB;
