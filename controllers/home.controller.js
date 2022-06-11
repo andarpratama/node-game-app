b2f4fc221a8221966a7690d02f3162c0435a85ce
andarpratama <andar.webdev@gmail.com>
2022-06-11 07:19:56 +0700

    add linkedin account

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,14 +1,19 @@
 class Home {
-  static home(req, res) {
-    res.status(201).json({
-      msg: 'Welcome to the game..',
-      title: 'Node Game App',
-      author: 'Andar Pratama',
-      detail_author: 'https://node-game-app.herokuapp.com/author',
-      version: 2.0,
-      repo: 'https://github.com/andarpratama/node-game-app',
-      slide: 'https://bit.ly/3s4CRjH',
-    });
+  static home(req, res, next) {
+    try {
+      res.status(201).json({
+        msg: 'Welcome to the game..',
+        title: 'Node Game App',
+        author: 'Andar Pratama',
+        detail_author: 'https://node-game-app.herokuapp.com/author',
+        version: 2.0,
+        repo: 'https://github.com/andarpratama/node-game-app',
+        slide: 'https://bit.ly/3s4CRjH',
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   }
 
   static author(req, res) {
