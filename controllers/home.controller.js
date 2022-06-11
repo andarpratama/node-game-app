cdb0b28b10f19cc3b51ff05ba416289932d73664
andarpratama <andar.webdev@gmail.com>
2022-06-11 07:07:12 +0700

    add linkedin account

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,25 +1,25 @@
-
 class Home {
-   static home(req, res) {
-        res.status(201).json({
-            msg: 'Welcome to the game..',
-            title: 'Node Game App',
-            author: 'Andar Pratama',
-            version: 2.0,
-            repo: 'https://github.com/andarpratama/node-game-app'
-        })
-   }
+  static home(req, res) {
+    res.status(201).json({
+      msg: 'Welcome to the game..',
+      title: 'Node Game App',
+      author: 'Andar Pratama',
+      version: 2.0,
+      repo: 'https://github.com/andarpratama/node-game-app',
+    });
+  }
 
-   static author(req, res){
-       res.status(201).json({
-           name: 'Andar Pratama',
-           address: 'Tangerang',
-           phone: '081283699257',
-           education: 'Bachelor Degree in Informatics Enginering',
-           instagram: '@andar.pratama_',
-           github: 'https://github.com/andarpratama'
-       })
-   }
+  static author(req, res) {
+    res.status(201).json({
+      name: 'Andar Pratama',
+      address: 'Tangerang',
+      phone: '081283699257',
+      education: 'Bachelor Degree in Informatics Enginering',
+      instagram: '@andar.pratama_',
+      github: 'https://github.com/andarpratama',
+      linkedin: 'https://www.linkedin.com/in/andarpratama/',
+    });
+  }
 }
 
 module.exports = Home;
